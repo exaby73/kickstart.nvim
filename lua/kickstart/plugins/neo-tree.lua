50ed5fe137ee97501272d500e9fe14f847a47dee
exaby73 <nabeel@ex3.dev>
2025-07-28 08:11:57 +0530

    feat: Auto refresh neo-tree on focus change

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
@@ -15,11 +15,40 @@ return {
   },
   opts = {
     filesystem = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
       },
     },
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function()
+          vim.cmd 'highlight! Cursor blend=100'
+        end,
+      },
+      {
+        event = 'neo_tree_buffer_leave',
+        handler = function()
+          vim.cmd 'highlight! Cursor blend=0'
+        end,
+      },
+    },
   },
+  config = function(_, opts)
+    require('neo-tree').setup(opts)
+
+    -- Auto refresh on focus
+    vim.api.nvim_create_autocmd('FocusGained', {
+      pattern = '*',
+      callback = function()
+        require('neo-tree.sources.manager').refresh()
+      end,
+    })
+  end,
 }
